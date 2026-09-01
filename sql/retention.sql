d44b541723e4c0beff8364162a21e753f2619df0
Anna Startseva <star.nyra@yandex.ru>
2026-09-01 16:43:23 +0300

    Добавить описание портфолио и первый SQL-запрос по retention

---
+++ sql/retention.sql
@@ -0,0 +1,11 @@
+-- Retention по месячным когортам
+with first_order as (
+    select customer_id,
+           date_trunc('month', min(order_date)) as cohort_month
+    from orders
+    group by 1
+)
+select cohort_month, count(distinct customer_id) as customers
+from first_order
+group by 1
+order by 1;
